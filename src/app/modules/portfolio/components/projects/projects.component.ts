c70ff767984af8bb884a0f0f370345b64ee21a08
jhonathannc <jhonathannc@gmail.com>
2023-12-30 19:20:48 -0300

    fix: added missing import module

--- src/app/modules/portfolio/components/projects/projects.component.ts
+++ src/app/modules/portfolio/components/projects/projects.component.ts
@@ -1,14 +1,14 @@
 import { Component, inject, signal } from '@angular/core';
 import { IProject } from '../../interface/IProject.interface';
 import { projects } from './projects';
-import { MatDialog } from '@angular/material/dialog';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { EDialogPanelClass } from '../../enum/EDialogPanelClass.enum';
 import { DialogProjectsComponent } from '../dialog/dialog-projects/dialog-projects.component';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [MatDialogModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss',
 })
