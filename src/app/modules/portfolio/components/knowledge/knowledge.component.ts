f3f5d6557ad24546b58066127599d73f9f44538d
jhonathannc <jhonathannc@gmail.com>
2023-12-30 17:02:51 -0300

    feat: created new component: KnowlegdeComponent

---
+++ src/app/modules/portfolio/components/knowledge/knowledge.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-knowledge',
+  standalone: true,
+  imports: [],
+  templateUrl: './knowledge.component.html',
+  styleUrl: './knowledge.component.scss',
+})
+export class KnowledgeComponent {}
