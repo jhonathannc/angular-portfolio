87b73e6a539311814f55421986f79849700f0f38
jhonathannc <jhonathannc@gmail.com>
2023-12-30 18:58:18 -0300

    feat: created DialogComponent

--- src/app/modules/portfolio/components/projects/projects.component.ts
+++ src/app/modules/portfolio/components/projects/projects.component.ts
@@ -1,6 +1,9 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { IProject } from '../../interface/IProject.interface';
 import { projects } from './projects';
+import { MatDialog } from '@angular/material/dialog';
+import { EDialogPanelClass } from '../../enum/EDialogPanelClass.enum';
+import { DialogProjectsComponent } from '../dialog/dialog-projects/dialog-projects.component';
 
 @Component({
   selector: 'app-projects',
@@ -10,5 +13,14 @@ import { projects } from './projects';
   styleUrl: './projects.component.scss',
 })
 export class ProjectsComponent {
+  #dialog = inject(MatDialog);
+
   public projects = signal<IProject[]>(projects);
+
+  public openDialog(data: IProject) {
+    this.#dialog.open(DialogProjectsComponent, {
+      data,
+      panelClass: EDialogPanelClass.PROJECTS,
+    });
+  }
 }
